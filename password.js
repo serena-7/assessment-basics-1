97aa44b711ded1d8e318999974deed52e3d0f0bc
Serena Sorensen <serena.sorensen7@gmail.com>
2022-03-25 10:29:25 -0600

    password validator added

--- password.js
+++ password.js
@@ -1,2 +1,20 @@
 //Password Validation System
 
+console.log("Welcome to password validation system!");
+
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+reader.question("Please Enter Password (Minimum of 10 Characters): ", function(input){
+    const password = input;
+    if(password.length > 9){
+        console.log("Congratulations! Password validated successfully!");
+    } else {
+        console.log("Password Validation Failed!");
+    }
+    reader.close();
+})
