cc458e20d6bfeceffd53833498228697ef1e7f07
Serena Sorensen <serena.sorensen7@gmail.com>
2022-03-25 11:15:05 -0600

    added additional password checks

--- password.js
+++ password.js
@@ -1,6 +1,6 @@
 //Password Validation System
 
-console.log("Welcome to password validation system!");
+console.log("Welcome to password validation system! \nPassword Requirements: \n- 10 characters or more \n- At least 1 upper case Letter \n- At least 1 lower case letter \n- At least 1 number \n- At least 1 special character *!@+#;:");
 
 const readline = require('readline');
 
@@ -9,12 +9,57 @@ const reader = readline.createInterface({
   output: process.stdout,
 });
 
-reader.question("Please Enter Password (Minimum of 10 Characters): ", function(input){
+reader.question("Please Enter Password: ", function(input){
     const password = input;
+    
+    let bLength = false;
+    let bCapLetter = false;
+    let bLowLetter = false;
+    let bNumber = false;
+    let bSpecial = false;
+    const specialChars = /[*!@+#;:]/;
+
     if(password.length > 9){
+        bLength = true;
+    }
+
+    for(let i = 0; i < password.length; i++){
+        if (!isNaN(password[i])) {
+            bNumber = true;
+        } else if(specialChars.test(password[i])) {
+            bSpecial = true;
+        } else if(password[i] === password[i].toUpperCase()){
+            bCapLetter = true;
+        } else if (password[i] === password[i].toLowerCase()) {
+            bLowLetter = true;
+        }
+    }
+
+    if (bLength && bNumber && bSpecial && bCapLetter && bLowLetter){
         console.log("Congratulations! Password validated successfully!");
     } else {
-        console.log("Password Validation Failed!");
+        console.log("Sorry the password could not be validated. \nErrors:");
+        if(!bLength){
+            console.log("Password is not at least 10 characters.");
+        }
+        if(!bCapLetter){
+            console.log("Does not contain at least 1 upper case letter.");
+        }
+        if(!bLowLetter){
+            console.log("Does not contain at least 1 lower case letter.");
+        }
+        if(!bNumber){
+            console.log("Does not contain at least 1 number.");
+        }
+        if(!bSpecial){
+            console.log("Does not contain at least 1 special character *!@+#;:")
+        }
     }
+
+    // if(password.length > 9){
+    //     console.log("Congratulations! Password validated successfully!");
+    // } else {
+    //     console.log("Password Validation Failed!");
+    // }
     reader.close();
 })
