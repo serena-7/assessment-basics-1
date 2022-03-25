ea3a8055661499b1d4a203e9397cb9de14627116
Serena Sorensen <serena.sorensen7@gmail.com>
2022-03-25 10:37:55 -0600

    all required problems complete

--- sldc.js
+++ sldc.js
@@ -19,4 +19,8 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
-const analysis = "";
+const analysis = "This is the second step, in which developers go over the requirements of their system and the project to determine how the code should be built and what the code should contain";
+const design = "This is the third step, in which developers design/build/create the code and system";
+const implementation = "This is the fourth step, in which the new code is implemented and put into working order ";
+const testing = "This is the fifth step, in which the implemented code is rigorously tested and then integrated into any existing systems and effectively made public";
+const maintain = "This is the final step, in which the existing code is maintained and updated to continue to meet the needs of the system";
